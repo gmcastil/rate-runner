5a691625904f27ad6c2b38b17e290b5dd1700c60
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-04-23 00:10:55 -0600

    Added integration test that uses local user configuration
    - Basic user configuration is added now and loads username and password
      from the default configuration.
    - If the configuration isn't created, then it will be skipped

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,8 +1,11 @@
-import os
 import pytest
+from pathlib import Path
 
+from rate_runner import constants
 from rate_runner.session import create_authenticated_session
 from rate_runner.session import build_login_payload
+from rate_runner.config import get_default_config_path
+from rate_runner.config import load_config_from_path
 
 def test_login_payload_format():
     payload = build_login_payload("myuser", "mypassword")
@@ -13,4 +16,32 @@ def test_login_payload_format():
     assert payload["__ac_name"] == "myuser"
     assert payload["__ac_password"] == "mypassword"
     assert all(isinstance(v, str) for v in payload.values())
+
+@pytest.mark.integration
+def test_create_login_session():
+
+    path = get_default_config_path()
+
+    # Users need to create their own configuration to run integration tests
+    if not path.exists():
+        pytest.skip(f"Configuraton file not found at {path} - skipping integration test")
+
+    try:
+        user_config = load_config_from_path()
+    except Exception as err:
+        pytest.fail(f"Failed to load configuration: {err}")
+
+    username = user_config["username"]
+    password = user_config["password"]
+    session = create_authenticated_session(username, password)
+    # URL that should only be accessible if login was successful
+    protected_url = f"{constants.CREME96_URL}/CREME-MC/Members/{username}"
+    response = session.get(protected_url)
+    response.raise_for_status()
+
+    # Use the presence of the 'Log out' option to determine if we were successful
+    assert "Log out" in response.text
+    # Full name is something that has to be supplied in the configuration, so it can be a
+    # reliable indicator of whether log in was successful
+    assert user_config["full_name"] in response.text
 
